6913d64dde0288a9b66ba0d5e58be158848e1143
Tornike Kharaishvili <tornike.kharaishvili@yahoo.com>
2016-11-02 21:43:01 +0400

    Project created and set up

---
+++ MaterialDesignInXamlToolkit.BugReport/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using MaterialDesignThemes.Wpf;
+using System.Windows;
+
+namespace MaterialDesignInXamlToolkit.BugReport
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            await DialogHost.Show(new MessageBox(), "TheIdentifier");
+        }
+    }
+}
